616d95d3d500c3e6cef175cff6c6b0ecb46ead19
kyr511 <yerin2k4@gmail.com>
2024-04-09 17:59:46 +0900

    1_Lab02

---
+++ 1_Lab02_01.c
@@ -0,0 +1,14 @@
+//11382 ²¿¸¶ Á¤¹Î
+#include <stdio.h>
+
+int main()
+{
+	long long A, B, C;
+	scanf("%lld", &A);
+	scanf(" %lld", &B);
+	scanf(" %lld", &C);
+
+	printf("%lld", A + B + C);
+
+	return 0;
+}
